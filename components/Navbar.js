5a8dbdcb24b63b432335efdd3349d214be88d2cf
smakur143 <souvikmakur143@gmail.com>
2024-10-26 14:40:19 +0530

    Started

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-[20%] bg-slate-500 h-screen p-6'>
+      <h1>Sidebar</h1>
+    </div>
+  )
+}
+
+export default Navbar
